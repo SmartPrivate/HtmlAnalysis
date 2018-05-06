96e4f5f58732ccaed60096da1b9c470577e9a7a9
alexma <alexma900807@gmail.com>
2018-05-06 21:24:26 +0800

    finish wechat parser and loader

--- HtmlParser.py
+++ HtmlParser.py
@@ -1,6 +1,6 @@
 import logging
 
-import requests
+import re
 from bs4 import BeautifulSoup
 from ContentData import WeChatArticleData
 from datetime import datetime
@@ -9,36 +9,40 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=loggin
 
 
 class WeChatParser(object):
+    def __init__(self, request_get):
+        self.soup = BeautifulSoup(request_get, 'lxml')
 
-    def __init__(self, url):
-        r = requests.get(url)
-        r.encoding = 'utf-8'
-        self.__soup = BeautifulSoup(r.text, 'lxml')
-        for script in self.__soup(["script", "style"]):
+    def get_soup(self):
+        pass
+
+
+class WeChatContentParser(WeChatParser):
+    def get_soup(self):
+        for script in self.soup(["script", "style"]):
             script.extract()
 
     def __get_article_title(self) -> str:
-        return self.__soup.title.text
+        return self.soup.title.text
 
     def __get_origin_tag(self) -> bool:
-        return self.__soup.find(id='copyright_logo') is not None
+        return self.soup.find(id='copyright_logo') is not None
 
     def __get_post_date(self) -> datetime:
-        date_str = self.__soup.find(id='post-date').text
+        date_str = self.soup.find(id='post-date').text
         result = datetime.strptime(date_str, '%Y-%m-%d')
         return result
 
     def __get_post_user_name(self) -> str:
-        return self.__soup.find(id='post-user').text
+        return self.soup.find(id='post-user').text
 
     def __get_post_user_id(self) -> str:
-        return self.__soup.find_all('span', 'profile_meta_value')[0].text
+        return self.soup.find_all('span', 'profile_meta_value')[0].text
 
     def __get_post_user_introduction(self) -> str:
-        return self.__soup.find_all('span', 'profile_meta_value')[1].text
+        return self.soup.find_all('span', 'profile_meta_value')[1].text
 
     def __get_article_content(self) -> str:
-        result = self.__soup.find_all(id='js_content')
+        result = self.soup.find_all(id='js_content')
         return '\t'.join(result)
 
     def get_parsed_article(self) -> WeChatArticleData:
@@ -51,3 +55,9 @@ class WeChatParser(object):
             self.__get_post_user_introduction(),
             self.__get_article_content()
         )
+
+
+class WeChatListParser(WeChatParser):
+
+    def get_soup(self):
+        return self.soup.find_all('a', uigs=re.compile('article_title_*'))
